d3b78d0587f902a3518aefc085b4b6c97ba70393
Evgenia0712 <e.gunko1992@mail.ru>
2023-07-05 23:44:28 +0300

    feat: добавляет решения 5,6 задач

--- index.js
+++ index.js
@@ -20,8 +20,63 @@ let copyGrades = [...grades];
 function sortAndPrint(a, b) {
   return a - b;
 }
-//console.log(copyGrades.sort(sortAndPrint));
+console.log(copyGrades.sort(sortAndPrint));
 console.log(copyGrades[11]);
 
 //Задание 4. Найдите и выведите минимальный балл среди студентов.
 console.log(copyGrades[0]);
+
+//Задание 5. Посчитайте и выведите количество студентов, получивших положительную оценку (балл выше или равен 60). Используйте
+//   метод массива, который фильтрует оценки и оставляет только те, которые соответствуют заданному условию. Затем
+//   посчитайте количество оставшихся оценок и выведите результат.
+const filteredGrades = grades.filter(el => el >= 60);
+//console.log(filteredGrades);
+console.log(filteredGrades.length);
+
+//Задание 6. Посчитайте и выведите количество студентов, получивших отрицательную оценку (балл ниже 60). Используйте
+//    аналогичный подход, как в предыдущем шаге.
+const filteredGradesBad = grades.filter(el => el < 60);
+//console.log(filteredGradesBad);
+console.log(filteredGradesBad.length);
+
+//Задание 7. Преобразуйте числовые оценки в формат буквенных оценок A/B/C/D/E, используя следующие правила:<br>
+//    - Если оценка находится в диапазоне от 80 до 100, преобразуйте её в "A"<br>
+//   - Если оценка находится в диапазоне от 60 до 79, преобразуйте её в "B"<br>
+//    - Если оценка находится в диапазоне от 40 до 59, преобразуйте её в "C"<br>
+//    - Если оценка находится в диапазоне от 20 до 39, преобразуйте её в "D"<br>
+//    - Если оценка ниже 20, преобразуйте её в "E"
+
+/*
+if (80 <= el <= 100) {
+  el = 'A';
+} else if (60 <= el <= 79) {
+  el = 'B';
+} else if (40 <= el <= 59) {
+  el = 'C';
+} else if (20 <= el <= 39) {
+  el = 'D';
+} else {
+  el = 'E';
+}
+*/
+
+const result1 = document.querySelector('.result1');
+result1.textContent = grades;
+
+const result2 = document.querySelector('.result2');
+result2.textContent = middleGrade;
+
+const result3 = document.querySelector('.result3');
+result3.textContent = copyGrades[11];
+
+const result4 = document.querySelector('.result4');
+result4.textContent = copyGrades[0];
+
+const result5 = document.querySelector('.result5');
+result5.textContent = filteredGrades.length;
+
+const result6 = document.querySelector('.result6');
+result6.textContent = filteredGradesBad.length;
+
+const result7 = document.querySelector('.result7');
+result7.textContent = grades;
